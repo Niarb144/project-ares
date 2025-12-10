986d21cda360f405ba8743a10b27745494a7d707
Teddy <teddybrian543@gmail.com>
2025-12-10 11:57:13 +0300

    Add Social Links

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -5,12 +5,12 @@ import { FaXTwitter } from "react-icons/fa6";
 
 export default function Footer() {
   return (
-    <footer className="absolute b-0 w-full flex flex-col items-center justify-center px-8 py-6 bg-gray-800 text-white mt-12"> 
+    <footer className="absolute b-0 w-full flex flex-col items-center justify-center px-8 py-6 bg-blue-400 text-white mt-12"> 
         <div className="mb-4 flex justify-center gap-4" id="socials">
-            <Link href="http://github.com" className="mx-3 hover:text-blue-400"><FaGithub /></Link>
-            <Link href="https://linkedin.com/in/teddybrian543" className="mx-3 hover:text-blue-400"><FaLinkedinIn /></Link>
-            <Link href="https://x.com/teddybrian543" className="mx-3 hover:text-blue-400"><FaXTwitter /></Link>
-            <Link href="https://instagram.com/teddybrian543" className="mx-3 hover:text-blue-400"><FaInstagram /></Link>    
+            <Link href="https://github.com/Niarb144" target="_blank" className="mx-3 hover:text-blue-400"><FaGithub /></Link>
+            <Link href="https://www.linkedin.com/in/teddy-brian-7a0057228/" target="_blank" className="mx-3 hover:text-blue-400"><FaLinkedinIn /></Link>
+            <Link href="https://x.com/BrianTeddy7" target="_blank" className="mx-3 hover:text-blue-400"><FaXTwitter /></Link>
+            <Link href="https://www.instagram.com/pendoria_/" target="_blank" className="mx-3 hover:text-blue-400"><FaInstagram /></Link>    
         </div>   
         <div className="text-sm">&copy; {new Date().getFullYear()} Teddy. All rights reserved.</div>
     </footer>
