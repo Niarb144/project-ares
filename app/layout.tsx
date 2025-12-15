779a35ad11d602f4d85a62f94464cc8619d8389d
Teddy <teddybrian543@gmail.com>
2025-12-15 11:47:09 +0300

    Fix scroll error

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
 
   return (
     <html lang="en">
-      <body className="bg-slate-950 overflow-hidden">
+      <body>
         <ClientLayout>
           {children}
         </ClientLayout>
