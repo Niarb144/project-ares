5dea611ba201fb1c0097eae3b19b47648bd83b67
Teddy <teddybrian543@gmail.com>
2025-12-18 11:16:04 +0300

    Edit Contact Page and section

--- app/contacts/page.tsx
+++ app/contacts/page.tsx
@@ -9,6 +9,9 @@ export default function ContactPage() {
     return (
         <main className="min-h-screen">
             <Navbar />
+            <section className="w-full h-[40vh] flex flex-col items-center justify-center px-8 py-20 bg-gray-300">
+                <h1 className="text-5xl font-bold mb-4 text-center">Let's <span>Chat</span></h1>
+            </section>
             <Contact />
             <Footer />
         </main>
--- components/TransitionProvider.tsx
+++ components/TransitionProvider.tsx
@@ -17,7 +17,7 @@ export default function TransitionProvider({
       initial={{ x: 40, opacity: 0 }}
       animate={{ x: 0, opacity: 1 }}
       transition={{
-        duration: 0.45,
+        duration: 0.55,
         ease: [0.22, 1, 0.36, 1],
       }}
       className="min-h-screen"
