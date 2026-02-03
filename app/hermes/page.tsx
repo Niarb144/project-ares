d30e11dc1c5bfefa2fe4a89cc634dca0f8708206
Teddy <teddybrian543@gmail.com>
2026-02-03 11:14:56 +0300

    Add Hermes Game

--- app/hermes/page.tsx
+++ app/hermes/page.tsx
@@ -5,12 +5,12 @@ import Image from "next/image";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
-export default function NidincPage() {
+export default function HermesPage() {
     return (
         <main className="min-h-screen">
             <Navbar />
             <section className="w-full h-[40vh] flex flex-col items-center justify-center px-8 py-20 bg-gray-300">
-                <h1 className="text-5xl font-bold mb-4 text-center">Nidinc Project</h1>
+                <h1 className="text-5xl font-bold mb-4 text-center">Hermes Project</h1>
             </section>
             <div className="max-w-6xl mx-auto px-6 py-12">
                 <motion.div
@@ -19,11 +19,7 @@ export default function NidincPage() {
                     transition={{ duration: 0.6, ease: "easeOut" }}
                     className="mb-8"
                 >
-                <ul className="list-disc list-inside space-y-4 text-lg text-gray-800">
-                    <li><a href="/tictactoe" className="text-blue-600 hover:underline" target="_blank" rel="noopener noreferrer">Tic Tac Toe</a></li>
-                    <li><a href="/hermes" className="text-blue-600 hover:underline" target="_blank" rel="noopener noreferrer">Hermes</a></li>
-                    <li><a href="/breakout" className="text-blue-600 hover:underline" target="_blank" rel="noopener noreferrer">Breakout Game</a></li>
-                </ul>   
+                <img src="/images/hermes1.png" alt="Hermes Project Screenshot" className="w-full h-auto rounded-lg shadow-lg" />
                 </motion.div>
             </div>
             <Footer />
